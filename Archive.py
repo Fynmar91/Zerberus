c956c4aa515f729a3ea2d4afbafbf155aae8ad35
Yannik Seitz <yannikseitz@outlook.de>
2019-04-30 19:36:08 +0200

    bug

--- Archive.py
+++ Archive.py
@@ -13,7 +13,7 @@ import door_class
 def main():
 	door = door_class.Door()
 	mail = mail_class.Mail()
-	logs = door.Query("SELECT * FROM Logs WHERE logID = %s", '*')
+	logs = door.Query("SELECT * FROM Logs WHERE logID = *", '*')
 	mail.SendArchive(logs, 'Logarchiv:')
 
 if __name__ == "__main__":
