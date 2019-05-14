b7606e9bea1942e760bb7c1e8d64dd7720661fc8
Yannik Seitz <yannikseitz@outlook.de>
2019-05-14 14:42:47 +0200

    1

--- Zerberus.py
+++ Zerberus.py
@@ -246,14 +246,15 @@ class SQL:
 
 	# Eigene IP finden
 	def get_ip(self):
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		try:
-			s.connect(('10.255.255.255', 1))
-			IP = s.getsockname()[0]
+			IP = socket.gethostbyname(socket.gethostname())
+#			s.connect(('10.255.255.255', 1))
+#			IP = s.getsockname()[0]
 		except:
 			IP = '127.0.0.1'
-		finally:
-			s.close()
+#		finally:
+#			s.close()
 		print(IP)
 		return IP
 
