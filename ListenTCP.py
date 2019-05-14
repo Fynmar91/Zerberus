7a40aee463e0061d7abb12b2dba49a1f56ac1f85
Yannik Seitz <yannikseitz@outlook.de>
2019-05-14 18:58:33 +0200

    1

--- ListenTCP.py
+++ ListenTCP.py
@@ -8,15 +8,15 @@ TCP_PORT = 8080
 # Normally 1024, but I want fast response
 BUFFER_SIZE = 20
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-
-conn, addr = s.accept()
-while 1:
-	data = conn.recv(BUFFER_SIZE)
-	if not data: break
-	Zerberus.Manual()
-	# echo
-	conn.send(data)
-conn.close()
+while True:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((TCP_IP, TCP_PORT))
+	s.listen(1)
+	conn, addr = s.accept()
+	while True:
+		data = conn.recv(BUFFER_SIZE)
+		if not data: break
+		Zerberus.Manual()
+		# echo
+		conn.send(data)
+	conn.close()
