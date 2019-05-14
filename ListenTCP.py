19d151407daa55c65f09b4fa44023a29b262130e
Yannik Seitz <yannikseitz@outlook.de>
2019-05-14 15:24:27 +0200

    1

--- ListenTCP.py
+++ ListenTCP.py
@@ -5,18 +5,18 @@ import Zerberus
 
 TCP_IP = '10.1.1.41'
 TCP_PORT = 8080
-BUFFER_SIZE = 20 # Normally 1024, but I want fast response
+# Normally 1024, but I want fast response
+BUFFER_SIZE = 20
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print ('Connection address:', addr)
 while 1:
 	data = conn.recv(BUFFER_SIZE)
 	if not data: break
 	Zerberus.Manual()
-	print ("received data:", data)
-	conn.send(data)  # echo
+	# echo
+	conn.send(data)
 conn.close()
--- SendTCP.py
+++ SendTCP.py
@@ -12,5 +12,3 @@ s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
-
-print ("received data:", data)
