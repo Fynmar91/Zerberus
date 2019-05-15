52d89718d34ca586170c043468c19be111d1f405
Yannik Seitz <yannikseitz@outlook.de>
2019-05-15 18:04:26 +0200

    1

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ from rpi_ws281x import *
 
 # LED strip configuration:
 LED_1_COUNT      = 1      # Number of LED pixels.
-LED_1_PIN        = 13      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
+LED_1_PIN        = 12      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
 LED_1_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_1_DMA        = 10      # DMA channel to use for generating signal (Between 1 and 14)
 LED_1_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
@@ -18,7 +18,7 @@ LED_1_CHANNEL    = 0       # 0 or 1
 LED_1_STRIP      = ws.SK6812_STRIP_GRBW
 
 LED_2_COUNT      = 0      # Number of LED pixels.
-LED_2_PIN        = 19      # GPIO pin connected to the pixels (must support PWM! GPIO 13 or 18 on RPi 3).
+LED_2_PIN        = 12      # GPIO pin connected to the pixels (must support PWM! GPIO 13 or 18 on RPi 3).
 LED_2_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_2_DMA        = 11      # DMA channel to use for generating signal (Between 1 and 14)
 LED_2_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
@@ -28,17 +28,12 @@ LED_2_STRIP      = ws.WS2811_STRIP_GRB
 
 def multiColorWipe(color1, color2, wait_ms=5):
 	global strip1
-	global strip2
 	"""Wipe color across multiple LED strips a pixel at a time."""
 	for i in range(strip1.numPixels()):
 		if i % 2:
 			# even number
 			strip1.setPixelColor(i, color1)
-			strip2.setPixelColor(i / 2, color2)
 			strip1.show()
-			time.sleep(wait_ms/1000.0)
-			strip2.show()
-			time.sleep(wait_ms/1000.0)
 		else:
 			# odd number
 			strip1.setPixelColor(i, color1)
@@ -55,17 +50,14 @@ def blackout(strip):
 if __name__ == '__main__':
 	# Create NeoPixel objects with appropriate configuration for each strip.
 	strip1 = Adafruit_NeoPixel(LED_1_COUNT, LED_1_PIN, LED_1_FREQ_HZ, LED_1_DMA, LED_1_INVERT, LED_1_BRIGHTNESS, LED_1_CHANNEL, LED_1_STRIP)
-	strip2 = Adafruit_NeoPixel(LED_2_COUNT, LED_2_PIN, LED_2_FREQ_HZ, LED_2_DMA, LED_2_INVERT, LED_2_BRIGHTNESS, LED_2_CHANNEL, LED_2_STRIP)
 
 	# Intialize the library (must be called once before other functions).
 	strip1.begin()
-	strip2.begin()
 
 	print ('Press Ctrl-C to quit.')
 
 	# Black out any LEDs that may be still on for the last run
 	blackout(strip1)
-	blackout(strip2)
 
 	while True:
 
